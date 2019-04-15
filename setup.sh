7429cf3ffd6f1bee5d195e8399ff7747db8163aa
ricke <rick.eihara@gmail.com>
2019-04-15 04:36:16 -0300

    Refact: Removing unnecessary lines

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt install -y \
-  vim-gnome build-essential git curl zsh tmux arandr cmake virtualbox # my programs
+  vim-gnome build-essential git curl zsh tmux arandr cmake virtualbox
 
 # Configuring vim
 mkdir ~/Documents/git
@@ -16,13 +16,7 @@ cp ~/Documents/git/dotfiles/.git* ~/
 
 vim -c ":PluginInstall" -c ":q" -c ":q"
 
-# Docker fingerprint
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://get.docker.com -o get-docker.sh | bash 
 
 # Rvm fingerprint
 gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
@@ -30,8 +24,6 @@ gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y \
-  apt-transport-https ca-certificates gnupg-agent software-properties-common \ #docker
-  docker-ce docker-ce-cli containerd.io \ # docker
   yarn nodejs # node js
 
 # Set shortcuts
