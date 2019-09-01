e9a912fca2fcb63e0f123756483151549e6a9836
root <root@DESKTOP-VKTSD01.localdomain>
2019-09-01 19:13:44 -0300

    Refact: Change apt to apt-get (safe for cli-commands)

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install -y \
+sudo apt-get update
+sudo apt-get install -y \
   vim-gnome build-essential git curl zsh tmux arandr cmake \
   virtualbox ctags ack-grep
 
@@ -23,9 +23,9 @@ curl -fsSL https://get.docker.com | bash
 # Rvm fingerprint
 gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 
-sudo apt update
-sudo apt upgrade -y
-sudo apt install -y \
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y \
   yarn nodejs
 
 # Installing golang
